346ad95fbd624f4360725fe89f5ef385cfb4479b
DivyanshRaj2006 <90976145+DivyanshRaj2006@users.noreply.github.com>
2022-07-27 17:59:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJANS704RqYjBTr4YF1MVkg5mRA0uFZis",
+  authDomain: "storytelling-5a8b6.firebaseapp.com",
+  databaseURL: "https://storytelling-5a8b6-default-rtdb.firebaseio.com",
+  projectId: "storytelling-5a8b6",
+  storageBucket: "storytelling-5a8b6.appspot.com",
+  messagingSenderId: "259308299633",
+  appId: "1:259308299633:web:019356105a0379ae1b82d0"
+};
